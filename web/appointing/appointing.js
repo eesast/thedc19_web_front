d16c7156f7926f89b5b5e40bf077aaffc94e57c2
lei-zhan17 <18810759221@163.com>
2018-09-20 21:59:21 +0800

    change appointing v1.2

--- web/appointing/appointing.js
+++ web/appointing/appointing.js
@@ -4,13 +4,15 @@
 	var c=document.getElementById('3');
 	var d=document.getElementById('4');
 	var e=document.getElementById('5');
-	var button=document.getElementById('6');
+	var button1=document.getElementById('6');
+	var button2 = document.getElementById('disappointing')
 	var t=document.getElementById('c1');
 	var time=new Array([5])
 	var number=new Array([5])
 	var p=0;
 	var q=0;
 	var i=0;
+	var flag = '';
 	for(i=0;i<5;i++){
 		number[i]=new Array([6])
 		time[i]=new Array([6]);
@@ -19,12 +21,56 @@
 				number[i][j]=0;
 			}
 	}
-	function hidden(){
-		document.getElementById('c1').style.visibility='hidden';
-		document.getElementById('c2').style.visibility='hidden';
-		document.getElementById('c3').style.visibility='hidden';
+	function hidden()//隐藏表格的函数
+	{
+		document.getElementById('c1').parentNode.style.visibility='hidden';
+		document.getElementById('c2').parentNode.style.visibility='hidden';
+		document.getElementById('c3').parentNode.style.visibility='hidden';
 	}
-	function choose(j,k){
+
+	function appointing(hour1,min1,hour2,min2)//显示约定的时间
+	{
+		if(hour1<9 || hour1>14 || !hour1){
+			return 0;
+		}
+		if(min1<0 || min1>60 || !min1){
+			if(!min1) min1=0
+			else return 0;
+		}
+		if(hour2<9 || hour2>14 || !hour2){
+			return 0;
+		}
+		if(min2<0 || min2>60 || !min2){
+			if(!min2) min2=0;
+			else return 0;
+		}
+		var div = document.createElement('div');
+		flag.parentNode.appendChild(div);
+		var wid0 = parseFloat(document.getElementsByClassName('time')[0].rows[0].cells[0].offsetWidth);
+		var wid2 = 1000-wid0;
+		var wid1 = ((hour2 + min2/60)-(hour1 + min1/60))/6;
+		div.style.width = wid1 * wid2+'px';
+		div.style.height = '50px';
+		div.style.position = 'absolute';
+		alert((((hour1 + min1/60)-9)/6)*wid2 + wid0)
+		div.style.marginLeft= ((((hour1 + min1/60)-9)/6)*wid2 + wid0) + 'px';
+		div.style.marginTop = '2px'
+		div.style.backgroundColor = 'cornflowerblue';
+		div.style.opacity = '0.5';//降低透明度
+		//div.innerText = hour1 + ':' + min1 + '~' + hour2 + ':' + min2;
+		div.style.lineHeight = '50px';
+		div.addEventListener('mouseover',function(){
+			div.style.opacity = '1';//回复透明度
+			div.innerText = hour1 + ':' + min1 + '~' + hour2 + ':' + min2;
+		})
+		div.addEventListener('mouseout',function(){
+			div.style.opacity = '0.5';//减低透明度
+			div.innerText = '';
+		})
+		div.className = 'add';
+	}
+	function choose(j,k)//选择时间按钮
+	{
 		if(time[j][k].style.backgroundColor=='white'){
 			time[j][k].style.backgroundColor='cornflowerblue';
 			number[j][k]=1;
@@ -34,7 +80,8 @@
 			number[j][k]=0;
 		}
 	}
-	function begin(){
+	function begin()//开始的初始化
+	{
 		for(p=0;p<3;p++)
 			{
 				for(q=0;q<6;q++)
@@ -51,10 +98,11 @@
 					}
 			}
 	}
-	function change(j,k)
+	function change(j,k)//切换表格
 	{
 		hidden();
-		document.getElementById(k).style.visibility='visible';
+		document.getElementById(k).parentNode.style.visibility='visible';
+		flag = document.getElementById(k);
 		a.style.backgroundColor='aliceblue';
 		b.style.backgroundColor='aliceblue';
 		c.style.backgroundColor='aliceblue';
@@ -75,40 +123,51 @@
         {
             //按下了确认
             //优先关闭窗口
-            
-           
             document.getElementsByClassName("dark")[0].style.display="none";//屏幕半黑
             document.getElementsByClassName("showinfor")[0].style.display="none";//弹框
-          
             if(callback && typeof(callback)==="function")
             {
                 callback();
                
             }
-           
         });
-
+		document.getElementsByClassName('no')[0].addEventListener('click',function()
+	{
+		//按下了取消
+        //优先关闭窗口
+        document.getElementsByClassName("dark")[0].style.display="none";//屏幕半黑
+        document.getElementsByClassName("showinfor")[0].style.display="none";//弹框
+	})
     }
 	begin();
 	hidden();
-	document.getElementById('c1').style.visibility='visible';
+	document.getElementById('c1').parentNode.style.visibility='visible';
+	flag = document.getElementById('c1');
 	a.style.backgroundColor='cornflowerblue';
 	a.addEventListener('click',function(){change(a,'c1')});
 	b.addEventListener('click',function(){change(b,'c2')});
 	c.addEventListener('click',function(){change(c,'c3')});
 	d.addEventListener('click',function(){change(d,'c3')});
 	e.addEventListener('click',function(){change(e,'c3')});
-	var str=""
-    for(i=0;i<5;i++)
-    {
-        for(var j=0;j<6;j++)
-        {
-            str+=number[i][j]+', ';
-        }
-    }
-    button.addEventListener('click',function(){showbox(str)})
-	// button.addEventListener('click',function(){showbox(number)})
-	
+	button1.addEventListener//预约按钮的功能实现
+	('click',function(){appointing(
+		parseInt(document.getElementById('input1').value),
+		parseInt(document.getElementById('input2').value),
+		parseInt(document.getElementById('input3').value),
+		parseInt(document.getElementById('input4').value)
+	)
+	})
+	button2.addEventListener//取消预约按钮的功能实现
+	('click',function(){showbox('确定要取消您所有的预约',
+		function(){
+			var my = document.getElementsByClassName('add');
+			for(var ti =0 ; ti<my.length;ti++)
+			{
+				my[ti].parentNode.removeChild(my[ti])
+			}
+		})
+	})
+
 	var mybody={'username':'admin','password':'eesast-software'}
 	var url='http://58.87.111.176/api/auth'
 	var myheaders = new Headers(
