0b061b0a78def0639007e6df69569afd9349720b
silentsaber <805898882@qq.com>
2018-09-22 00:10:57 +0800

    第一次对接登录

--- web/newpage/jointeam.js
+++ web/newpage/jointeam.js
@@ -345,4 +345,19 @@ function init()//初始化，从服务器读取已有队伍信息并显示
            // document.getElementById("name").focus();
         })
      }
+     function setCookie(cname,cvalue){
+        // var d = new Date();
+        // d.setTime(d.getTime()+(exdays*24*60*60*1000));
+        // var expires = "expires="+d.toGMTString();
+        document.cookie = cname+"="+cvalue;
+    }
+    function getCookie(cname){
+        var name = cname + "=";
+        var ca = document.cookie.split(';');
+        for(var i=0; i<ca.length; i++) {
+            var c = ca[i].trim();
+            if (c.indexOf(name)==0) { return c.substring(name.length,c.length); }
+        }
+        return "";
+    }
     
