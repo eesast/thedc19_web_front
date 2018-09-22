fbd9dd56977578ae5c1650170341d537092a8192
lei-zhan17 <18810759221@163.com>
2018-09-22 17:20:44 +0800

    change appointing v1.4

--- web/appointing/appointing.js
+++ web/appointing/appointing.js
@@ -1,129 +1,101 @@
 // JavaScript Document
-	var a=document.getElementById('1');
-	var b=document.getElementById('2');
-	var c=document.getElementById('3');
-	var d=document.getElementById('4');
-	var e=document.getElementById('5');
 	var button1=document.getElementById('6');
 	var button2 = document.getElementById('disappointing')
 	var t=document.getElementById('c1');
-	var time=new Array([5])
-	var number=new Array([5])
 	var p=0;
 	var q=0;
 	var i=0;
 	var showtime='';
-	var day = document.getElementById('1')
+	var days = '';
+	var day = document.getElementById('demo')
 	var flag = document.getElementById('c1');
 	var space = document.getElementById('1-space1')
-	for(i=0;i<5;i++){
-		number[i]=new Array([6])
-		time[i]=new Array([6]);
-		for(var j=0;j<6;j++)
-			{
-				number[i][j]=0;
-			}
-	}
-	function hidden()//隐藏表格的函数
-	{
-		document.getElementById('c1').parentNode.style.visibility='hidden';
-		document.getElementById('c2').parentNode.style.visibility='hidden';
-		document.getElementById('c3').parentNode.style.visibility='hidden';
-	}
-
 	function appointing(hour1,min1,hour2,min2,mark)//显示约定的时间,mark用来表示是否为用户添加的时间
 	{
-		if(hour1<9 || hour1>14 || !hour1){
-			return 0;
-		}
-		if(min1<0 || min1>60 || !min1){
-			if(!min1) min1=0
-			else return 0;
-		}
-		if(hour2<9 || hour2>14 || !hour2){
-			return 0;
-		}
-		if(min2<0 || min2>60 || !min2){
-			if(!min2) min2=0;
-			else return 0;
-		}
-		if(min1<10 || min2 <10) showtime = hour1 + ':' + '0' + min1 + '~' + hour2 + ':' + '0' + min2;
-		else showtime = hour1 + ':' + min1 + '~' + hour2 + ':' + min2;
+		var sign = 1;
+		//清空输入框
 		document.getElementById('input1').value='';
 		document.getElementById('input2').value='';
 		document.getElementById('input3').value='';
-		document.getElementById('input4').value='';		
-		var div = document.createElement('div');
-		flag.parentNode.appendChild(div);
-		var wid0 = parseFloat(document.getElementsByClassName('time')[0].rows[0].cells[0].offsetWidth);
-		var wid2 = 1000-wid0;
-		var wid1 = ((hour2 + min2/60)-(hour1 + min1/60))/6;
-		div.style.width = wid1 * wid2+'px';
-		div.style.height = '50px';
-		div.style.position = 'absolute';
-		div.style.marginLeft= ((((hour1 + min1/60)-9)/6)*wid2 + wid0) + 'px';
-		div.style.marginTop = '2px'
-		div.style.backgroundColor = 'cornflowerblue';
-		div.style.opacity = '0.5';//降低透明度
-		//div.innerText = hour1 + ':' + min1 + '~' + hour2 + ':' + min2;
-		div.style.lineHeight = '50px';
-		document.getElementById('showtime').innerText ='您预约的时间和地点：' + day.innerText + showtime + flag.rows[0].cells[0].innerText;
-		div.addEventListener('mouseover',function(){
-		div.style.opacity = '1';//回复透明度
-		div.innerText =showtime;
-		})
-		div.addEventListener('mouseout',function(){
-		div.style.opacity = '0.5';//减低透明度
-		div.innerText = '';
-		})
-		if(mark) div.className = 'add';
-	}
-	function white(){
-		for(p=1;p<4;p++)
-		{
-			for(q=1;q<3;q++)
-				{
-					var st=String(p)+'-place'+String(q);
-					document.getElementById(st).style.backgroundColor = 'white';
-				}
+		document.getElementById('input4').value='';
+		//判断是否合理
+		if(min1<0 || min1>60 || !min1){
+			if(!min1) min1=0
+			else{showbox('输入错误'); return 0;}
 		}
-	}
-	function choose(j)//选择时间按钮
-	{
-		white();
-		j.style.backgroundColor='cornflowerblue';
-		space = j;
-	}
-	function begin()//开始的初始化
-	{
-		for(p=1;p<4;p++)
-			{
-				for(q=1;q<3;q++)
-					{
-						var st=String(p)+'-place'+String(q);
-						var id1=p;
-						var id2=q;
-						if(q==1) document.getElementById(st).style.backgroundColor = 'cornflowerblue';
-						else document.getElementById(st).style.backgroundColor = 'white';
-						document.getElementById(st).addEventListener('click',function(){
-							//alert('click'+this.getAttribute('id'))
-							choose(this)
-						});
-					}
-			}
-	}
-	function change(j,k)//切换表格
-	{
-		hidden();
-		document.getElementById(k).parentNode.style.visibility='visible';
-		flag = document.getElementById(k);
-		day = j;
-		a.style.backgroundColor='aliceblue';
-		b.style.backgroundColor='aliceblue';
-		c.style.backgroundColor='aliceblue';
-		d.style.backgroundColor='aliceblue';
-		e.style.backgroundColor='aliceblue';
-		j.style.backgroundColor='cornflowerblue';
+		if(min2<0 || min2>60 || !min2){
+			if(!min2) min2=0;
+			else{showbox('输入错误'); return 0;}
+		}
+		//判断时间是否是分段的
+		var h1=hour1 + min1/60;
+		var h2=hour2 + min2/60;
+		if(hour1>=hour2 || hour1<0 || hour2>24 || hour2-hour1>2 || !hour1 || !hour2 ){ showbox('输入错误');return 0}
+		else{ if(h1<12 && h2<=12){
+			sign = 0;
+			var st=0;
+		}
+		if(h1>=12 && h2>12) {
+			sign = 1;
+			var st = 12;
+		}
+		if(h1<12 && h2>12){
+			sign = 2
+		}
+		}
+		//调整输出时间格式
+		var showmin1 = min1.toString()
+		var showmin2 = min2.toString()
+		if(min1<10) showmin1 = '0' + showmin1;
+		if(min2<10) showmin2 = '0' + showmin2;
+		showtime =  hour1 + ':' + showmin1 + '~' + hour2 + ':' + showmin2;
+		//根据时间是否分段来渲染
+		if(sign == 2){
+			var st=0;
+			choose(hour1,12,min1,0,mark,0);
+			st = 12;
+			choose(12,hour2,0,min2,mark,1);
+		}
+		else choose(hour1,hour2,min1,min2,mark,sign);
+		days = day.value;
+		//渲染函数
+		function choose(hour1,hour2,min1,min2,mark,t)
+		{	//添加子div
+			var div = document.createElement('div');
+			div.style.fontSize = '10px';
+			document.getElementById('c1').parentNode.appendChild(div);
+			//计算所选时间的长度的占比
+			var wid0 = parseFloat(document.getElementsByClassName('time')[0].rows[0].cells[0].offsetWidth);
+			var wid2 = 1000-wid0;
+			var wid1 = ((hour2 + min2/60)-(hour1 + min1/60))/12;
+			//改变div的属性
+			div.style.width = wid1 * wid2+'px';
+			div.style.height = '50px';
+			div.style.position = 'absolute';
+			div.style.marginLeft= ((((hour1 + min1/60)-st)/12)*wid2 + wid0) + 'px';
+			div.style.marginTop = '3px'
+			div.style.backgroundColor = 'cornflowerblue';
+			div.style.opacity = '0.5';//降低透明度
+			//div.innerText = hour1 + ':' + min1 + '~' + hour2 + ':' + min2;
+			if(t) div.style.marginTop = '56px'
+			div.style.lineHeight = '50px';
+			//添加div的显示监听 
+			div.addEventListener('mouseover',function(){
+				div.style.opacity = '1';//回复透明度
+				if(min1<10 && min2 <10) div.innerText = hour1 + ':' + '0' + min1 + '~' + hour2 + ':' + '0' + min2;
+				if(min1<10 && min2>=10) div.innerText = hour1 + ':' + '0' + min1 + '~' + hour2 + ':' + min2;
+				if(min1>=10 && min2<10) div.innerText = hour1 + ':' + min1 + '~' + hour2 + ':' + '0' + min2;
+				if(min1>=10 && min2>=10) div.innerText  = hour1 + ':' + min1 + '~' + hour2 + ':' + min2;
+			})
+			div.addEventListener('mouseout',function(){
+				div.style.opacity = '0.5';//减低透明度
+				div.innerText = '';
+			})
+			if(mark) div.className = 'add';
+			//上传预约数据
+			upload(username,days,hour1,hour2,min1,min2)
+		}
+		document.getElementById('showtime').innerText ='您预约的时间和地点：' + day.value + '号' + showtime + flag.rows[0].cells[0].innerText;
 	}
 	function showbox(s,callback)//打印一段话
     {
@@ -152,45 +124,119 @@
 		document.getElementsByClassName("showinfor")[0].style.display="none";//弹框
 	})
     }
-	begin();
-	hidden();
+	document.getElementById('1-place1').style.backgroundColor = 'cornflowerblue';
+	document.getElementById('1-place2').style.backgroundColor = 'cornflowerblue';
 	document.getElementById('c1').parentNode.style.visibility='visible';
-	a.style.backgroundColor='cornflowerblue';
-	a.addEventListener('click',function(){change(a,'c1')});
-	b.addEventListener('click',function(){change(b,'c2')});
-	c.addEventListener('click',function(){change(c,'c3')});
-	d.addEventListener('click',function(){change(d,'c3')});
-	e.addEventListener('click',function(){change(e,'c3')});
 	button1.addEventListener//预约按钮的功能实现
-	('click',function(){appointing(
+	('click',function(){
+		appointing(
 		parseInt(document.getElementById('input1').value),
 		parseInt(document.getElementById('input2').value),
 		parseInt(document.getElementById('input3').value),
 		parseInt(document.getElementById('input4').value),
-		1
-	)})
+		1)
+	})
 	button2.addEventListener//取消预约按钮的功能实现
 	('click',function(){showbox('确定要取消您所有的预约',
 		function(){
 			var my = document.getElementsByClassName('add');
-			for(var ti = 0 ; ti<my.length;ti++)
+			for(var ti = 0 ; ti<my.length; ti++)
+			{
+				my[ti].parentNode.removeChild(my[ti])
+			}
+			for(var ti = 0 ; ti<my.length; ti++)
 			{
 				my[ti].parentNode.removeChild(my[ti])
 			}
 			document.getElementById('showtime').innerText ='您预约的时间和地点：' ;
 		})
 	})
+	//显示预约情况
+	document.getElementById('showappointment').addEventListener('click',function(){
+		update();
+	})
+	//后端交互
 
-	var mybody={'username':'admin','password':'eesast-software'}
-	var url='http://58.87.111.176/api/auth'
-	var myheaders = new Headers(
-		{
-			contentType: "application/x-www-form-urlencoded",
+	function setCookie(cname,cvalue){
+		// var d = new Date();
+		// d.setTime(d.getTime()+(exdays*24*60*60*1000));
+		// var expires = "expires="+d.toGMTString();
+		document.cookie = cname+"="+cvalue+";path=/";
+	}
+	function getCookie(cname){
+		var name = cname + "=";
+		var ca = document.cookie.split(';');
+		for(var i=0; i<ca.length; i++) {
+			var c = ca[i].trim();
+			if (c.indexOf(name)==0) { return c.substring(name.length,c.length); }
 		}
-	)
-	fetch(url,{method:'POST',
-			   headers:myheaders,
-			   body:mybody
-			  }).then(response=>{
-				  console.log(response.body)
-			  }).catch(error=>{console.log('error')})
+		return "";
+	}
+
+	function update(day)//获取当前日期的预约情况函数
+	{
+		fetch('https://58.87.111.176/api/sites/:id/appointments',{
+			headers:{
+				'Content-Type':'application/json',
+				'x-access-token':token,
+				'query':day
+			}
+		}).then(response=>{
+			var start = response.json();
+			for(var ti=0;ti<start.length;ti++)//将获取的时间数据交给appointing函数渲染
+			{
+					var hour1 =parseInt(start[0]['startTime'].substring(11,13));
+					var min1 =parseInt(start[0]['startTime'].substring(14,15));
+					var hour1 =parseInt(start[0]['endTime'].substring(11,13));
+					var min1 =parseInt(start[0]['endTime'].substring(14,15));
+					appointing(
+						hour1,min1,hour2,min2,0
+					)
+		}})
+	}
+	function upload(name,days,hour1,hour2,min1,min2)//上传预约数据函数
+	{
+		var st = days + 'T' + hour1 + ':' + min1 +':00.000Z';
+		var et = days + 'T' + hour2 + ':' + min2 +':00.000Z'
+		var body1 = {
+			'teamId':name,
+			'startTime':st,
+			'endtime':et
+		}
+		fetch('https://58.87.111.176/api/sites/:id/appointments',{
+			method:'POST',
+			headers:{
+				'Content-Type':'application/json',
+				'x-access-token':token
+			},
+			body:JSON.stringify(body1)
+		}).then(response=>{
+			if(response.ok) showbox("预约成功")//上传成功提示
+			if(response.status=409) showbox('预约时间冲突')//时间冲突提示
+		})
+	}
+	var token=getCookie('token');
+	var username=getCookie('username');//登陆后传入
+
+	//以下为以前版本
+	//fetch('https://58.87.111.176/api/sites/:id',{
+	//	headers:
+	//	{
+	//		'Content-Type':'application/json',
+	//		'x-access-token':token
+	//	}
+	//}).then(response=>{
+	//	var start = response.json();
+	//	for(var ti=0;ti<start.length;ti++)
+	//	{
+	//		var days = start[0]['startTime'].substring(0,10);
+	//		var element1 = document.getElementById(days)
+	//		var hour1 =parseInt(start[0]['startTime'].substring(11,13));
+	//	var min1 =parseInt(start[0]['startTime'].substring(14,15));
+	//		var hour1 =parseInt(start[0]['endTime'].substring(11,13));
+	//		var min1 =parseInt(start[0]['endTime'].substring(14,15));
+	//		appointing(
+	//			element1,hour1,min1,hour2,min2,0
+	//		)
+	//	}
+	//})
