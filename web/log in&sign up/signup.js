2d69bf0e84e134e41c33e96a9a9f25f5850e7965
gesy <gesy17@mails.tsinghua.edu.cn>
2018-09-22 22:57:29 +0800

    add secure check

--- web/log in&sign up/signup.js
+++ web/log in&sign up/signup.js
@@ -23,8 +23,8 @@ var userinfo={
 
 //将用户输入信息保存在userinfo中，与服务器中数据对比
 var btn=document.getElementById("submit")
-btn.addEventListener("click",name_checking)
-function name_checking(){
+btn.addEventListener("click",main)
+function checking(){
     userinfo.username=document.getElementById("username").value
     userinfo.password=document.getElementById("pwd").value
     userinfo.email=document.getElementById("email").value
@@ -33,6 +33,77 @@ function name_checking(){
     userinfo.phone=document.getElementById("phone").value
     userinfo.realname=document.getElementById("realname").value
     userinfo.studentId=document.getElementById("thuid").value
+    if(checkrealname(userinfo.realname)===false)
+    {
+        showbox("真实姓名必须为1-5个汉字！请重新输入")
+        var changerealname=document.getElementById("realname")
+        changerealname.value=null
+        return false
+    }
+    if(checkphone(userinfo.phone)===false)
+    {
+        showbox("手机号输入不合法！请重新输入")
+        var changephone=document.getElementById("phone")
+        changephone.value=null
+        return false
+    }
+    if(checkId(userinfo.studentId)===false)
+    {
+        showbox("学号输入不合法！请重新输入")
+        var changestudentid=document.getElementById("thuid")
+        changestudentid.value=null
+        return false
+    }
+    if(checkemail(userinfo.email)===false)
+    {
+        showbox("邮箱输入不合法！请重新输入")
+        var changeemail=document.getElementById("email")
+        changeemail.value=null
+        return false
+    }
+    return true
+}
+
+function main(){
+    if(checking()===true)
+    {
+        senddata()
+    }
+}
+
+function checkphone(obj) {
+    var reg = /^\d{11}$/ ;
+     if (reg.test(obj)) {
+       return true
+     }
+     return false
+   }
+
+function checkId(obj) {
+   var reg = /^\d{10}$/ ;
+    if (reg.test(obj)) {
+      return true
+    }
+    return false
+  }
+
+function checkemail(obj) {
+    var reg = /^([a-zA-Z0-9]+[_|\_|\.]?)*[a-zA-Z0-9]+@([a-zA-Z0-9]+[_|\_|\.]?)*[a-zA-Z0-9]+\.[a-zA-Z]{2,3}$/;
+    if (reg.test(obj)) {
+     return true
+    }
+    return false
+}
+
+function checkrealname(obj) {
+    var reg = /^[\u4E00-\u9FA5]{1,5}$/;
+     if (reg.test(obj)) {
+       return true
+     }
+     return false
+   }
+
+function senddata(){
     fetch("http://58.87.111.176/api/users",
     {
         method:'POST',
