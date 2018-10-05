896aa7f184156412e79bb58fd105c68845ad1b48
silentsaber <805898882@qq.com>
2018-10-05 19:40:18 +0800

    测试

--- web/newpage/main.js
+++ web/newpage/main.js
@@ -28,7 +28,7 @@ var userid=getCookie("userid");
             document.getElementById("userinfor1").style.display="none";
             document.getElementById("userinfor2").style.display="block";
             document.getElementById("userinfor2").innerHTML="您好，用户:<p id='user'><span style='cursor:pointer'>"+username+'</span></p>';
-            document.getElementById("user").style.color="gray";
+            document.getElementById("user").style.color="#ffffff";
             document.getElementById("user").addEventListener("click",function()
             {
 
